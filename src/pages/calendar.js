70f2fc4e71257e3e48de3c6f2b0e62fc6ce721b3
monadnoc <jordannelson1.2014@u.northwestern.edu>
2019-12-04 12:20:57 -0600

    Links logo draft, basic coalition info, first two pages

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -7,8 +7,8 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Calendar</h1>
+    <p>This is a calendar of the coalition's events and events relevant to cannabis policy and industry.</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
