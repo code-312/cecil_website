a431a24017c8e68a3e2fa7370737b019578ecf9b
Jordan Nelson <jordannelson1.2014@u.northwestern.edu>
2019-12-26 23:36:19 -0600

    Wording updates as requested by CECIL

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,9 +22,10 @@ const IndexPage = () => (
       }}>
         <img src={image}/> */}
         <h1>Who are we?</h1>
-        <p>We are a group of community members and non-profit organizations fighting to make sure the legalization of cannabis Illinois is done to repair and reinvest in the communities most impacted by the war on drugs, and to ensure the cannabis industry prioritizes people over profits!</p>
-        <p>We meet weekly at 6PM at the <a style={{color:'#10AC84'}} target="_blank" rel="noreferrer noopener" href="https://www.google.com/maps/place/The+Port+Ministries/@41.80262,-87.6703387,17z/data=!3m1!4b1!4m5!3m4!1s0x880e2e6f5c66cf51:0x3e4628c759874afc!8m2!3d41.80262!4d-87.66815">Port Ministries</a> in Chicago's Back of the Yards Neighborhood</p>
-        <p>Our next meeting with be in January, 2020</p>
+        <p>We are a coalition made up of community members and non-profit organizations fighting to make sure the legalization of cannabis Illinois is done to repair and reinvest in the communities most impacted by the war on drugs, and to ensure the cannabis industry prioritizes people over profits!</p>
+        <p>We have weekly meetings during the evenings in neighborhoods disproportionately affected by the War on Drugs. We do this to ensure community voice is heard!</p>
+        <p>We meet weekly at 6PM on Thursdays.</p>
+        <p>Our next meeting with be on January 9th, 2020 at the <a style={{color:'#10AC84'}} target="_blank" rel="noreferrer noopener" href="https://www.google.com/maps/place/The+Port+Ministries/@41.80262,-87.6703387,17z/data=!3m1!4b1!4m5!3m4!1s0x880e2e6f5c66cf51:0x3e4628c759874afc!8m2!3d41.80262!4d-87.66815">Port Ministries</a> in Chicago's Back of the Yards Neighborhood</p>
         <Image/>
       {/* </div> */}
     {/* </div> */}
